f51a184b0735fc53c1281d2396d4884b53c53ade
Miltenis <darens.miltenis@sk.lvt.lv>
2025-06-08 20:52:14 +0300

    Izveidots programmas logs.

---
+++ Tests.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+#--------------------------------- programmas logs ------------------------------#
+root = tk.Tk()
+root.title("Tests")
+root.geometry("800x500")  
+root.resizable(False, False) 
+root.configure(bg='#EEEEEE')
+
+root.mainloop()
